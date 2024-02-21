d26aee3a2336ffafb7b6e1a8e16739b7347103fe
JarlissonTomaz <tomazj995@gmail.com>
2024-02-21 13:43:03 -0300

    variables 2

---
+++ fundamentos/variaveis_2.dart
@@ -0,0 +1,15 @@
+main() {
+  var n1 = 2;
+  var n2 = 4.56;
+  var texto = "abc";
+  //texto = 3;
+
+  print(n1 + n2);
+
+  print(n1.runtimeType);
+  print(n2.runtimeType);
+  print(texto.runtimeType);
+
+  print(n1 is int);
+  print(n2 is String);
+}
