561a4ad1fb34d85173c789df33eea5e48ad88c13
JarlissonTomaz <tomazj995@gmail.com>
2024-02-23 17:44:37 -0300

    generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,12 @@
+main() {
+  List<String> frutas = ['banana', 'maçã', 'uva'];
+  frutas.add('melão');
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 19500.00,
+    'vendedor': 1500.00,
+    'estagiário': 600.00
+  };
+  print(salarios);
+}
