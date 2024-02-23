ea924c588f485c5d30dfdc590efa7a6c31a88a19
JarlissonTomaz <tomazj995@gmail.com>
2024-02-23 18:33:41 -0300

    challenge if else

---
+++ controle/desafio_if_else.dart
@@ -0,0 +1,10 @@
+main() {
+  var nota = 2.0;
+
+  if (nota >= 9) // ; cuidado com o ';', não usar com as estruturas de controle
+  {
+    print('parabens, voce foi brilhante!');
+  }
+
+  //exceçâo do erro é do while
+}
