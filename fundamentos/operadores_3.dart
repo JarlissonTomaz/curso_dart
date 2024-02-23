91c39ff364103be670a67ea5847b1dc3d9a3d955
JarlissonTomaz <tomazj995@gmail.com>
2024-02-23 10:58:49 -0300

    operators 3

---
+++ fundamentos/operadores_3.dart
@@ -0,0 +1,19 @@
+main() {
+  int a = 3;
+  int b = 4;
+
+  //Operadores Unários
+  a++; // posfix
+  --a; // prefix
+
+  print(a);
+
+  print(a++ == --b);
+  print(a == b);
+//Operador Lógico Unário (NOT)
+  print(!true);
+  print(!false);
+
+  bool x = false;
+  print(!x);
+}
