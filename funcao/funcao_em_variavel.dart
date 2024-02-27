13c566f8320a5aac7c42df2b7d1021366c41f75a
JarlissonTomaz <tomazj995@gmail.com>
2024-02-27 15:11:33 -0300

    function in variable 1

---
+++ funcao/funcao_em_variavel.dart
@@ -0,0 +1,18 @@
+int sumFn(int a, int b) {
+  return (a + b);
+}
+
+main() {
+  int a = 2;
+  int b = 3;
+
+  int Function(int, int) sum1 = sumFn;
+
+  int Function(int, int) sum2 = (int x, int y) {
+    return x + y;
+  };
+
+  print(sum2(a, b));
+
+  print(sum1(a, b));
+}
