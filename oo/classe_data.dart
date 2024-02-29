1740268e2ca1cf817b8dd46bd8e9acab33ad6f3a
JarlissonTomaz <tomazj995@gmail.com>
2024-02-29 08:29:14 -0300

    date class v.4

--- oo/classe_data.dart
+++ oo/classe_data.dart
@@ -5,6 +5,13 @@ class Data {
 
   Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
 
+  Data.com({this.dia = 1, this.mes = 1, this.ano = 1970});
+
+  Data.primeiroDiaDoAno(this.ano) {
+    dia = 1;
+    mes = 1;
+  }
+
   String dataFormatada() {
     return '${dia}/${mes}/${ano}';
   }
@@ -16,9 +23,14 @@ class Data {
 
 main() {
   Data data = Data(12, 3, 2020);
-  data.dia = 10;
-  data.mes = 3;
-  data.ano = 2021;
-  print(Data(12, 3, 1989));
+  // data.dia = 10;
+  // data.mes = 3;
+  // data.ano = 2021;
   print(data);
+
+  print(Data(12, 3, 1989));
+
+  print(Data.com(dia: 12, mes: 3, ano: 1989));
+
+  print(Data.primeiroDiaDoAno(2022));
 }
